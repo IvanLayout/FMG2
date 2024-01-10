4496a141e26ba9a10e244c81eb6691470f996112
Ivan <ivanpetrachenkov.web@gmail.com>
2024-01-10 11:46:27 +0100

    first commit

---
+++ js/functions.js
@@ -0,0 +1,276 @@
+$(() => {
+	// Есть ли поддержка тач событий или это apple устройство
+	if (!is_touch_device() || !/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform)) $('html').addClass('custom_scroll')
+
+	// Observer API
+	const boxes = document.querySelectorAll('.lazyload, .lazymap')
+
+	function scrollTracking(entries) {
+		for (const entry of entries) {
+			if (entry.intersectionRatio > 0 && entry.target.getAttribute('data-src') && !entry.target.classList.contains('loaded')) {
+				entry.target.src = entry.target.getAttribute('data-src')
+
+				entry.target.classList.add('loaded')
+			}
+
+			if (entry.intersectionRatio > 0 && entry.target.getAttribute('data-map') && !entry.target.classList.contains('loaded-map')) {
+				entry.target.classList.add('loaded-map')
+
+				addScriptsURL('https://api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects&lang=ru-RU')
+			}
+		}
+	}
+
+	const observer = new IntersectionObserver(scrollTracking, {
+		threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+	})
+
+	boxes.forEach(element => observer.observe(element))
+	
+
+
+	// Установка ширины стандартного скроллбара
+	$(':root').css('--scroll_width', widthScroll() + 'px')
+
+
+	// Плавная прокрутка к якорю
+	$('.scroll-btn').click(function(e) {
+		e.preventDefault()
+
+		let href = $(this).data('anchor'),
+		addOffset = $('.header').innerHeight()
+
+		if ($(this).closest('.header__menu')){
+			$('.mob-menu-btn').removeClass('_active')
+			$('.header__menu').removeClass('_show')
+			$('body').removeClass('_menu-open')
+		}
+
+		$('html, body').stop().animate({ scrollTop: $(href).offset().top - addOffset }, 1000)
+	})
+
+
+	// Кастомный select
+	$('select').niceSelect()
+
+	// Маска ввода
+	$('input[type=tel]').each(function(){
+		let datamask = $(this).data('mask');
+
+		$(this).inputmask(`${datamask}`, {
+			showMaskOnHover: false
+		})
+	})
+
+
+	// Кнопка 'Вверх'
+	$('body').on('click', '.button-up', function(e) {
+		e.preventDefault()
+
+		$('body, html').stop(false, false).animate({
+			scrollTop: 0
+		}, 1000)
+	})
+
+	$('body').on('click', '.accordion__title', function (e) {
+		e.preventDefault()
+
+		if ($(this).closest('.accordion__item').hasClass('_active')) {
+			$(this).closest('.accordion__item').removeClass('_active')
+		} else {
+			$(this).closest('.accordion__item').addClass('_active')
+		}
+	})
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.placeFocusBack = false
+
+	Fancybox.defaults.template = {
+		closeButton: '<svg viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg"><rect y="27.7803" width="39" height="4" transform="rotate(-45 0 27.7803)" fill="#414042"/><rect x="2.82837" width="39" height="4" transform="rotate(45 2.82837 0)" fill="#FDBB30"/></svg>',
+	}
+
+	// Всплывающие окна
+	$('body').on('click', '.modal-btn', function (e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: $(this).data('content'),
+			type: 'inline'
+		}])
+	})
+
+	$('body').on('click', '.modal-close', function (e) {
+		e.preventDefault()
+
+		Fancybox.close()
+	})
+
+
+	// Увеличение картинки
+	Fancybox.bind('.fancy-img', {
+		Image: {
+			zoom: false,
+		},
+		Thumbs: {
+			autoStart: false,
+		}
+	})
+
+
+	// Выбор файла
+	$('.file-selection input[type=file]').change(function(){
+		var val = $(this).val()
+
+		var parent = $(this).parents('.file-selection')
+
+		parent.find('.file-selection__path').text(val)
+
+		if(parent.find('.file-selection__path').text() == '') {
+			parent.find('.file-selection__path').text('Добавить файл')
+		}
+	})
+})
+
+
+$(window).on('load', () => {
+	// Шапка
+	if( $(window).scrollTop() > 0 ) {
+		$('.header').addClass('fixed')
+	} else{
+		$('.header').removeClass('fixed')
+	}
+
+	if( $(window).scrollTop() > $(window).height() ) {
+		$('.page-anchor').addClass('_fixed')
+	} else{
+		$('.page-anchor').removeClass('_fixed')
+	}
+
+	$(window).scroll(function(){
+		if( $(window).scrollTop() > 0 ) {
+			$('.header').addClass('fixed')
+		} else{
+			$('.header').removeClass('fixed')
+		}
+
+		if( $(window).scrollTop() > $(window).height() ) {
+			$('.page-anchor').addClass('_fixed')
+		} else{
+			$('.page-anchor').removeClass('_fixed')
+		}
+
+		if( $(window).scrollTop() > ($('.footer').offset().top - $(window).height()) ) {
+			$('.btns-fix').addClass('bot')
+			$('.btns-fix').css('margin-bottom', $('.footer').innerHeight())
+		} else {
+			$('.btns-fix').removeClass('bot')
+			$('.btns-fix').css('margin-bottom', 0)
+		}
+	})
+})
+
+
+// Вспомогательные функции
+const widthScroll = () => {
+	let div = document.createElement('div')
+
+	div.style.overflowY = 'scroll'
+	div.style.width = '50px'
+	div.style.height = '50px'
+	div.style.visibility = 'hidden'
+
+	document.body.appendChild(div)
+
+	let scrollWidth = div.offsetWidth - div.clientWidth
+	document.body.removeChild(div)
+
+	return scrollWidth
+}
+
+function setHeight(className){
+    let maxheight = 0
+
+    className.each(function() {
+		let elHeight = $(this).outerHeight()
+
+        if( elHeight > maxheight ) {
+			maxheight = elHeight
+        }
+    })
+
+    className.outerHeight( maxheight )
+}
+
+const is_touch_device = () => !!('ontouchstart' in window)
+
+
+function addScriptsURL(url) {
+	let jsLoad = document.querySelector(".js-load")
+
+	if (jsLoad) {
+		var src = document.createElement('script')
+		src.src = url
+		jsLoad.appendChild(src)
+
+		src.addEventListener('load', () => {
+			mapInit()
+		});
+	}
+}
+
+
+// map
+function mapInit(){
+	ymaps.ready(() => {
+		if ( $(window).width() > 1024 ) {
+			var iconW = 101
+			var iconH = 185
+
+			var iconWO = 50*-1
+			var iconHO = 90*-1
+		} else if ($(window).width() > 1024) {
+			var iconW = 42
+			var iconH = 78
+
+			var iconWO = 21*-1
+			var iconHO = 39*-1
+		} else {
+			var iconW = 32
+			var iconH = 60
+
+			var iconWO = 16*-1
+			var iconHO = 30*-1
+		}
+
+		var myMap = new ymaps.Map("map", {
+			center: [55.796326778940575, 60.10939410767465],
+			zoom: 5
+		}),
+		myPlacemark = new ymaps.Placemark([59.92752726746504, 30.295593877897847], {}, {
+			iconImageHref: 'images/marker.png',
+			iconImageSize: [iconW, iconH],
+			iconImageOffset: [iconWO, iconHO],
+		}),
+		myPlacemark2 = new ymaps.Placemark([55.75153047107064, 37.61765893389772], {}, {
+			iconImageHref: 'images/marker.png',
+			iconImageSize: [iconW, iconH],
+			iconImageOffset: [iconWO, iconHO],
+		}),
+		myPlacemark3 = new ymaps.Placemark([55.796326778940575, 49.10939410767465], {}, {
+			iconImageHref: 'images/marker.png',
+			iconImageSize: [iconW, iconH],
+			iconImageOffset: [iconWO, iconHO],
+		}),
+		myPlacemark4 = new ymaps.Placemark([54.983107656536184, 82.89616436257408], {}, {
+			iconImageHref: 'images/marker.png',
+			iconImageSize: [iconW, iconH],
+			iconImageOffset: [iconWO, iconHO],
+		})
+
+		myMap.geoObjects.add(myPlacemark).add(myPlacemark2).add(myPlacemark3).add(myPlacemark4)
+	})
+}
---
+++ js/scripts.js
@@ -0,0 +1,301 @@
+// Ширина окна для ресайза
+WW = window.innerWidth || document.clientWidth || document.querySelector('body')[0].clientWidth
+
+// Моб. версия
+fakeResize = false
+fakeResize2 = true
+
+if (document.body.clientWidth < 375) {
+	document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+}
+
+$(() => {
+	$('body').on('click', '.mob-menu-btn', function (e) {
+		e.preventDefault()
+
+		if ($('.mob-menu-btn').hasClass('_active')) {
+			$('.mob-menu-btn').removeClass('_active')
+			$('.header').removeClass('_show')
+			$('.mob-header').removeClass('_hide')
+			$('body').removeClass('_menu-open')
+		} else {
+			$('.mob-menu-btn').addClass('_active')
+			$('.header').addClass('_show')
+			$('.mob-header').addClass('_hide')
+			$('body').addClass('_menu-open')
+		}
+	})
+
+
+	if ($('.brands__slider').length) {
+		new Swiper(".brands__slider", {
+			loop: false,
+			spaceBetween: 34,
+			slidesPerView: 'auto',
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			}
+		})
+	}
+
+
+	if ($('.partners__slider').length) {
+		new Swiper(".partners__slider", {
+			loop: false,
+			spaceBetween: 48,
+			slidesPerView: 'auto',
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			}
+		})
+	}
+
+
+	//
+	if ($('.main-reviews__slider').length) {
+		mainPeviewsSlider = new Swiper('.main-reviews__slider', {
+			loop: true,
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			spaceBetween: 20,
+			slidesPerView: 1,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			on: {
+				slideChange: function (swiper) {
+					$(swiper.$el).closest('.main-reviews').find('.main-reviews__list-item').removeClass('active')
+
+					$(swiper.$el).closest('.main-reviews').find(`.main-reviews__list-item:eq(${swiper.realIndex})`).addClass('active')
+				}
+			}
+		})
+
+		$('body').on('click', '.main-reviews__list-item', function(e) {
+			e.preventDefault()
+
+			let numberSlide = $(this).data('index-slide');
+			mainPeviewsSlider.slideToLoop(numberSlide);
+		})
+	}
+
+
+	if ($('.main-galllery__slider').length) {
+		mainGalllery = new Swiper(".main-galllery__slider", {
+			spaceBetween: 0,
+			slidesPerView: "auto",
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			keyboard: {
+				enabled: true,
+				onlyInViewport: true,
+				pageUpDown: true
+			},
+			mousewheel: {
+				sensitivity: 1,
+			},
+			nested: true,
+		})
+	}
+
+
+	if ($('.product-info__photo').length) {
+		productInfoBrands = new Swiper(".product-info__photo", {
+			loop: false,
+			spaceBetween: 25,
+			slidesPerView: 3,
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			rewind: true,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			breakpoints: {
+				'320': {
+					spaceBetween: 25,
+					slidesPerView: 3,
+				},
+				'480': {
+					spaceBetween: 25,
+					slidesPerView: 4,
+				},
+				'768': {
+					spaceBetween: 25,
+					slidesPerView: 5,
+				},
+				'1025': {
+					spaceBetween: 25,
+					slidesPerView: 5,
+				},
+				'1200': {
+					spaceBetween: 40,
+					slidesPerView: 5,
+				},
+				'1400': {
+					spaceBetween: 50,
+					slidesPerView: 5,
+				}
+			},
+		})
+
+		productInfoPhoto = new Swiper(".product-info__photo-slider", {
+			loop: false,
+			spaceBetween: 20,
+			slidesPerView: 1,
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			preloadImages: false,
+			rewind: true,
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false
+			},
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			thumbs: {
+				swiper: productInfoBrands
+			}
+		})
+	}
+});
+
+
+$(window).on('load', () => {
+	//
+	if ( $('.partners__grid').length ) {
+		$('.partners__grid').each(function() {
+			partnersHeight($(this), parseInt($(this).css('--partners_number')))
+		})
+	}
+
+	//
+	if ( $('.our-vacancy__items').length ) {
+		$('.our-vacancy__items').each(function() {
+			vacancyHeight($(this), parseInt($(this).css('--vacancy_number')))
+		})
+	}
+});
+
+
+$(window).on('resize', () => {
+	let windowW = window.outerWidth
+
+	if (typeof WW !== 'undefined' && WW != windowW) {
+		// Перезапись ширины окна
+		WW = window.innerWidth || document.clientWidth || document.querySelector('body')[0].clientWidth
+
+		// Моб. версия
+		if (!fakeResize) {
+			fakeResize = true
+			fakeResize2 = false
+
+			document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+		}
+
+		if (!fakeResize2) {
+			fakeResize2 = true
+
+			if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+		} else {
+			fakeResize = false
+			fakeResize2 = true
+		}
+
+
+		//
+		if ( $('.partners__grid').length ) {
+			$('.partners__grid').each(function() {
+				partnersHeight($(this), parseInt($(this).css('--partners_number')))
+			})
+		}
+
+		//
+		if ( $('.our-vacancy__items').length ) {
+			$('.our-vacancy__items').each(function() {
+				vacancyHeight($(this), parseInt($(this).css('--vacancy_number')))
+			})
+		}
+	}
+});
+
+
+// 
+function partnersHeight(context, step) {
+	let start    = 0
+	let finish   = step
+	let partners = context.find('.partners__item')
+
+	partners.find('.partners__title').height('auto')
+
+	for (let i = 0; i < partners.length; i++) {
+		setHeight(partners.find('.partners__title').slice(start, finish))
+
+		start  = start + step
+		finish = finish + step
+	}
+}
+
+// 
+function vacancyHeight(context, step) {
+	let start    = 0
+	let finish   = step
+	let vacancy = context.find('.our-vacancy__item')
+
+	vacancy.find('.our-vacancy__info').height('auto')
+	vacancy.find('.our-vacancy__title').height('auto')
+
+	for (let i = 0; i < vacancy.length; i++) {
+		setHeight(vacancy.find('.our-vacancy__info').slice(start, finish))
+		setHeight(vacancy.find('.our-vacancy__title').slice(start, finish))
+
+		start  = start + step
+		finish = finish + step
+	}
+}
